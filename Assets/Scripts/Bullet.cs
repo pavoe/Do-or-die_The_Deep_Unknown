4160f71b4067e7d1d6c53a257ecff95d8690b627
p.tuzinowski <p.tuzinowski@o2.pl>
2019-12-12 21:11:57 +0100

    Shooting mechanism implementation

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    //public int damage = 40;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed; //przemieszczanie się pocisku
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        //Debug.Log(hitInfo.name); //wyświetlanie w konsoli informacji o tym, na jaki collider natrafia pocisk
+
+        //ZADAWANIE OBRAŻEŃ WROGOWI
+        //Enemy enemy = hitInfo.GetComponent<Enemy>();
+        //if (enemy != null)  //jeśli znaleźliśmy komponent <Enemy>
+        //{
+        //    enemy.TakeDamage(damage); //zadajemy obrażenia
+        //}
+
+        Destroy(gameObject); //niszczy pocisk po trafieniu w coś
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3")) // UnityScene\Edit\ProjectSettings\Input - u mnie ustawiony jest lewy Shift
+            Shoot();
+        //zmienimy to na spację po zmianie poruszania się - po zmianie skakania ze spacji na W lub "up"
+    }
+
+    /// <summary>
+    /// Shooting mechanism.
+    /// </summary>
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); //chcemy jedynie zespawnować pocisk
+    }
+}
