4bd0ce1a9624dc746c674882427d02e123bfa264
Paulina <veuillons@gmail.com>
2020-01-07 02:13:27 +0100

    Trying to go further and checking if maybe something works problems: declaration of the enemies list (because there is no enemy type, but I need to think a bit about that) Raycast: I took two Vector3, left and right. Assuming the enemies can only see/detect the ones in that linear direction Don't know if I correctly took the distance between a center of the camera view and the enemy, and if "follow distance" can be used here (that is the part where I search if enemy is in camera view. Probably some much simpler solution, don't know it yet though)

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -5,21 +5,48 @@ using UnityEngine;
 public class AIController : MonoBehaviour
 {
     public GameObject enemy = GameObject.Find("Enemy");
-    public GameObject player = GameObject.Find("Player");
+    public GameObject player = GameController.gameController.PC;
+    List<enemy> enemies = new List<enemy>(); //Jakoś inaczej tę deklarację klasy, enemy nie jest typem, więc trzeba inaczej (ale nie wiem jeszcze jak)
+    private Vector3 cvcedistance = focusObject.transform.position - enemy.transform.position;
+    public float pcedistance = Vector3.Distance(player.transform.position, enemy.transform.position);
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < GameObject.Find("Enemies").transform.childCount; i++)
+        { 
+           enemies.Add(GameObject.Find("Enemies").transform.GetChild(i).gameObject); 
+        } 
     }
 
     // Update is called once per frame
+    private void CheckLineOfFire(GameObject enemy)
+    {
+        //sprawdzam czy odległość gracza od enemy jest odpowiednia
+        //sprawdza co ma, jeśli gracz jest na linii strzału woła  
+        //strzał. 
+        //Coś w stylu  
+        //enemy.GetComponent<Weapon>().Shoot(); 
+        //tutaj powinny być te Raycasty
+        if (pcedistance<=30)
+        {
+            //Tutaj Raycastować
+            if (Physics.RayCast(transform.position, Vector3.right, 30)||Physics.RayCast(transform.position, Vector3.left, 30)) //zrobić to eleganciej
+            {
+                enemy.GetComponent<Weapon>().Shoot();
+            }
+        }
+
+    } 
     void Update()
     {
-        float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
-        if (distance <= 30)
+        if (cvcedistance<=CameraController.followDistance)
         {
-            Shoot();
+            foreach(GameObject enemy in enemies)
+            {
+                CheckLineOfFire(enemy);
+            }
         }
+        //float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
         
         //Nie wiem, może da się coś zrobić, żeby Shoot dla Weapon działało też tu, public przy weapon nie zadziałało
     }
