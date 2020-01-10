7e831417c8cb9f90ddf241bbf07c15f19c131f67
Michał <miszust@gmail.com>
2020-01-10 14:05:03 +0100

    AI 1st stage

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -4,25 +4,69 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
-    public GameObject enemy = GameObject.Find("Enemy");
-    public GameObject player = GameObject.Find("Player");
+
+    List<GameObject> enemies = new List<GameObject>(); //Jakoś inaczej tę deklarację klasy, enemy nie jest typem, więc trzeba inaczej (ale nie wiem jeszcze jak)
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < GameObject.Find("Enemies").transform.childCount; i++)
+        { 
+           enemies.Add(GameObject.Find("Enemies").transform.GetChild(i).gameObject); 
+        } 
     }
 
     // Update is called once per frame
+    private void CheckLineOfFire(GameObject enemy)
+    {
+        Vector3 direction = new Vector3();
+        Vector3 origin = enemy.transform.position;
+
+        if (enemy.transform.position.x > GameController.gameController.PC.transform.position.x) //sprawdza czy gracz jest po lewej czy prawej
+        {
+            direction = Vector3.left;
+            origin -= (new Vector3(enemy.GetComponent<BoxCollider2D>().size.x, 0));
+        }
+        else
+        {
+            direction = Vector3.right;
+            origin += (new Vector3(enemy.GetComponent<BoxCollider2D>().size.x, 0));
+
+        }
+
+        
+
+
+
+
+
+        RaycastHit2D hitinfo = Physics2D.Raycast(origin, direction);
+
+        if(enemy.name=="Enemy1 (1)")
+        {
+            Debug.Log(hitinfo.collider);
+        }
+
+        //Tutaj Raycastować
+        if (hitinfo.transform.name=="Player") //zrobić to eleganciej
+        {
+            Debug.Log(enemy.name);
+            enemy.GetComponent<Weapon>().Shoot();
+        }
+
+
+    } 
     void Update()
     {
-        float distance = Vector3.Distance(player.transform.position, enemy.transform.position);
-        if (distance <= 30)
+        foreach (GameObject enemy in enemies)
         {
-            Shoot();
+            if (enemy.GetComponentInChildren<SpriteRenderer>().isVisible)
+            {
+                CheckLineOfFire(enemy);
+            }
         }
-        
-        //Nie wiem, może da się coś zrobić, żeby Shoot dla Weapon działało też tu, public przy weapon nie zadziałało
+
     }
     
 }
-
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject quitMenu;
+    public GameObject pauseMenu;
+    public GameObject quitMenuAfterPause;
+    public Button btnPlay;
+    public Button btnExit;
+    private GameObject mainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.menu = this;
+
+        foreach (Transform child in transform)
+        {
+            switch (child.name)
+            {
+                case "MainMenu":
+                    mainMenu = child.gameObject;   //pobranie menu głównego
+                    break;
+                case "QuitMenu":
+                    quitMenu = child.gameObject; //pobranie menu wyjścia z gry
+                    break;
+                case "PauseMenu":
+                    pauseMenu = child.gameObject; //pobranie menu pauzy
+                    break;
+                case "QuitMenuAfterPause":
+                    quitMenuAfterPause = child.gameObject; //pobranie menu wyjścia z gry po pauzie
+                    break;
+            }
+
+
+        }
+        btnPlay = btnPlay.GetComponent<Button>();
+        btnExit = btnExit.GetComponent<Button>();
+        quitMenu.SetActive(false);//wyłączenie menu wyjścia z gry
+        pauseMenu.SetActive(false);
+        quitMenuAfterPause.SetActive(false);
+        Time.timeScale = 0; //zatrzymanie czasu
+        Cursor.visible = mainMenu.activeSelf; //odkrycie kursora myszy
+        //Cursor.lockState = CursorLockMode.Confined; //odblokowanie kursora myszy  TEMPORARY COMMENTED (everywhere below also)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void btnPlayPressed()
+    {
+        mainMenu.SetActive(false);
+        Time.timeScale = 1; //włączenie czasu
+        Cursor.visible = false; //ukrycie kursora
+        //Cursor.lockState = CursorLockMode.Locked; //zablokowanie kursora
+        btnPlay.enabled = true; //aktywacja przycisku btnPlay
+    }
+
+    public void btnExitPressed()
+    {
+        quitMenu.SetActive(true); //aktywacja menu wyjścia
+        mainMenu.SetActive(false); //dezaktywacja menu głównego
+        btnPlay.enabled = false; //dezaktywacja btnPlay
+        btnExit.enabled = false; //dezaktywacja btnExit
+    }
+
+    public void btnYesExitPressed()
+    {
+        Application.Quit(); //wyjście z gry
+    }
+
+    public void btnCancelExitPressed()
+    {
+        mainMenu.SetActive(true); //aktywacja menu głównego
+        quitMenu.SetActive(false); //dezaktywacja menu wyjścia
+        btnPlay.enabled = true; //aktywacja btnPlay
+        btnExit.enabled = true; //aktywacja btnExit
+    }
+
+    public void showPauseMenu()
+    {
+        pauseMenu.SetActive(true); //aktywacja menu pauzy
+        Time.timeScale = 0; //zatrzymanie czasu
+        Cursor.visible = true; //odkrycie kursora
+        //Cursor.lockState = CursorLockMode.Confined; //odblokowanie kursora
+    }
+
+    public void btnResumeInPauseMenu()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+    }
+
+    public void btnRestartInPauseMenu()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void btnExitInPauseMenu()
+    {
+        quitMenuAfterPause.SetActive(true); //aktywacja menu wyjścia po pauzie
+        pauseMenu.SetActive(false); //dezaktywacja menu pauzy
+    }
+
+    public void btnYesInQuitMenuAfterPause()
+    {
+        Application.Quit();
+    }
+
+    public void btnCancelInQuitMenuAfterPause()
+    {
+        pauseMenu.SetActive(true); //aktywania menu pauzy
+        quitMenuAfterPause.SetActive(false); //dezaktywacja menu wyjścia po pauzie
+    }
+}
