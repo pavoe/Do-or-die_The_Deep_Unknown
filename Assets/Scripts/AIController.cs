7b0b0bb8dd393501d568c5312cf7f36f4684abfb
Paulina <veuillons@gmail.com>
2020-02-20 02:15:48 +0100

    Going into moving - AI
    Changing directions  doesnt work properly, overall not smooth.

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -18,15 +18,18 @@ public class AIController : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void CheckLineOfFire(GameObject enemy)
+
+    void CheckLineOfFire(GameObject enemy)
     {
         Vector3 direction = new Vector3();
         Vector3 origin = enemy.transform.position;
 
         if (enemy.transform.position.x > GameController.gameController.PC.transform.position.x) //sprawdza czy gracz jest po lewej czy prawej
         {
+
             enemy.transform.rotation = Quaternion.Euler(0,0,0);
             direction = Vector3.left;
+
             origin -= (new Vector3(enemy.GetComponent<BoxCollider2D>().size.x, 0));
         }
         else
@@ -52,15 +55,53 @@ public class AIController : MonoBehaviour
         }
 
 
-    } 
-    void Update()
+    }
+    
+    public float speed = 3f;
+    private Rigidbody2D rigidBody;
+    public Transform startPos, endPos;
+    private bool collision;
+    void Move()
+    {
+        rigidBody.velocity = new Vector2(transform.localScale.x, 0) * speed;
+        
+    }
+    void ChangeDirection()
+    {
+        collision = Physics2D.Linecast(startPos.position, endPos.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (!collision)
+        {
+            Vector3 temp = transform.localScale;
+            if (temp.x == 1.0)
+            {
+                temp.x = -1.0f;
+            }
+            else
+                temp.x = 1.0f;
+            transform.localScale = temp;
+        }
+    }
+   
+    //problemy: start pos i end pos ustawione dla wszystkich, rigidbody access do konkentnego (probkem, bo ten skrypt podpięty jest do gamecontrollera, a nie do wroga) i daltego get component tak działa
+    void FixedUpdate()
     {
         foreach (GameObject enemy in enemies)
         {
-            if (enemy.GetComponentInChildren<SpriteRenderer>().isVisible)
-            {
-                CheckLineOfFire(enemy);
-            }
+            rigidBody = enemy.GetComponent<Rigidbody2D>();
+            
+            //if (enemy.GetComponentInChildren<SpriteRenderer>().isVisible)
+            //{
+               
+            //    CheckLineOfFire(enemy);
+           // }
+            
+            Move();
+            ChangeDirection();
+            
+            
+            
+            //może tu spróbować umieścić to chodzenie
+
         }
 
     }
